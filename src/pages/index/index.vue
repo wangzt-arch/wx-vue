9fd157843ff7dfdd27f1d489e3755db4c5d96028
wzt <1213332180@qq.com>
2024-02-27 11:44:09 +0800

    chore: first

---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="index">
+    <view class="index">
+        11111111111111
+  </view>
+  </view>
+</template>
+
+<script>
+// 按需引入, 更小的应用体积
+import './index.scss'
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      msg: 'Hello world!',
+      show: false
+    }
+  },
+  methods: {
+    handleClick () {
+      this.show = true
+    },
+    handleClose () {
+      this.show = false
+    }
+  },
+}
+</script>
